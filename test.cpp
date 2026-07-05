c23c10d0d7daa2e19093421ec7fd781de7e6f122
Mehlail Abbas <mehlailabbas@gmail.com>
2026-07-05 12:12:46 +0500

    add file something

---
+++ test.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+int main() {
+    try {
+        int a = 10, b = 0;
+        if (b == 0)
+            throw runtime_error("Cannot divide by zero!");
+        cout << a / b;
+    }
+    catch (const runtime_error& e) {
+        cout << "Error: " << e.what() << endl;
+    }
+    return 0;
+}
+
+
+
